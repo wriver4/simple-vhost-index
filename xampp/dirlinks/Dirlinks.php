7552782030fca1f21267e75548baec647764321e
wriver4 <wriver4@users.noreply.github.com>
2018-06-09 12:31:36 -0400

    Update to make more linux friendly

--- xampp/dirlinks/Dirlinks.php
+++ xampp/dirlinks/Dirlinks.php
@@ -8,38 +8,38 @@
 class Dirlinks
 {
 
-    function show($path)
-    {
-        $dir_iterator = new RecursiveDirectoryIterator($path,
-                RecursiveDirectoryIterator::SKIP_DOTS);
-        $directory_count = 0;
-        foreach ($dir_iterator as $directory)
-        {
-            if ($directory->isDir() === TRUE and $directory->getFilename() !== "dirlinks" and $directory->getFilename()
-                    !== "nbproject")
-            {
-                $directory = end(explode(DIRECTORY_SEPARATOR, $directory));
-                $directory_count = $directory_count + 1;
-                if ($directory_count == 1)
-                {
-                    echo "<div class=\"container\"><div class=\"column-a\">";
-                }
-                echo "<li><a href='$directory' target=\"_blank\">$directory</a></li>";
+    function show($path) {
+		$dir_iterator = new RecursiveDirectoryIterator($path,
+			RecursiveDirectoryIterator::SKIP_DOTS);
+		$directory_count = 0;
+		$not = ['dirlinks', 'nbproject', 'local', 'lost+found', '.Trash-0', '.Trash-1000'];
+		$ext = ['com', 'info', 'net', 'dev', 'local'];
+		$ds = DIRECTORY_SEPARATOR;
 
-                if ($directory_count == 12)
-                {
-                    echo "</div><div class=\"column-b\">";
-                }
-                if ($directory_count == 24)
-                {
-                    echo "</div></div>";
-                    $directory_count = 0;
-                }
-            }
-        }
-        echo"</div></div>";
-    }
-
+		foreach ($dir_iterator as $directories) {
+			if ($directories->isDir() === TRUE and in_array($directories->getExtension(), $ext) and !in_array($directories->getFilename(), $not) and !$directories->isLink()) {
+				$directoryin = end(explode($ds, $directories));
+				$dirArray[] = $directoryin;
+			}
+		}
+		sort($dirArray);
+		foreach ($dirArray as $directory) {
+			$directory_count = $directory_count + 1;
+			if ($directory_count == 1) {
+				echo '<div class="container"><div class="column-a">';
+			}
+			echo "<li><a href=http://$directory target=\"_blank\">$directory</a></li>";
+			if ($directory_count == 12) {
+				echo '</div><div class="column-b">';
+			}
+			if ($directory_count == 24) {
+				echo "</div></div>";
+				$directory_count = 0;
+			}
+		}
+		echo "</div></div>";
+	}
+    
     function header($path, $host)
     {
         echo '<!DOCTYPE html><html lang="en"><head><meta charset="utf-8"><meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" /><title>'
